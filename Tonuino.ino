160550d2e53b3b82791d7fc60cbf165fd62d8645
Thorsten Hersam <thorsten.hersam@exxcellent.de>
2019-05-30 11:41:34 +0200

    Card can be configured even if we check if card is still there

--- Tonuino.ino
+++ Tonuino.ino
@@ -268,58 +268,56 @@ void loop() {
 
     Serial.println("Reading Card");
     if (readCard(&myCard) == true) {
-         mp3.playFolderTrack(1, 1);
-
       
-//      if (myCard.cookie == 322417479 && myCard.folder != 0 && myCard.mode != 0) {
-//        knownCard = true;
-//        _lastTrackFinished = 0;
-//        numTracksInFolder = mp3.getFolderTrackCount(myCard.folder);
-//        Serial.print(numTracksInFolder);
-//        Serial.print(F(" Dateien in Ordner "));
-//        Serial.println(myCard.folder);
-//  
-//        // Hörspielmodus: eine zufällige Datei aus dem Ordner
-//        if (myCard.mode == 1) {
-//          Serial.println(F("Hörspielmodus -> zufälligen Track wiedergeben"));
-//          currentTrack = random(1, numTracksInFolder + 1);
-//          Serial.println(currentTrack);
-//          mp3.playFolderTrack(myCard.folder, currentTrack);
-//        }
-//        // Album Modus: kompletten Ordner spielen
-//        if (myCard.mode == 2) {
-//          Serial.println(F("Album Modus -> kompletten Ordner wiedergeben"));
-//          currentTrack = 1;
-//          mp3.playFolderTrack(myCard.folder, currentTrack);
-//        }
-//        // Party Modus: Ordner in zufälliger Reihenfolge
-//        if (myCard.mode == 3) {
-//          Serial.println(
-//              F("Party Modus -> Ordner in zufälliger Reihenfolge wiedergeben"));
-//          currentTrack = random(1, numTracksInFolder + 1);
-//          mp3.playFolderTrack(myCard.folder, currentTrack);
-//        }
-//        // Einzel Modus: eine Datei aus dem Ordner abspielen
-//        if (myCard.mode == 4) {
-//          Serial.println(
-//              F("Einzel Modus -> eine Datei aus dem Odrdner abspielen"));
-//          currentTrack = myCard.special;
-//          mp3.playFolderTrack(myCard.folder, currentTrack);
-//        }
-//        // Hörbuch Modus: kompletten Ordner spielen und Fortschritt merken
-//        if (myCard.mode == 5) {
-//          Serial.println(F("Hörbuch Modus -> kompletten Ordner spielen und "
-//                           "Fortschritt merken"));
-//          currentTrack = EEPROM.read(myCard.folder);
-//          mp3.playFolderTrack(myCard.folder, currentTrack);
-//        }
-//      }
-//
-//      // Neue Karte konfigurieren
-//      else {
-//        knownCard = false;
-//        setupCard();
-//      }
+      if (myCard.cookie == 322417479 && myCard.folder != 0 && myCard.mode != 0) {
+        knownCard = true;
+        _lastTrackFinished = 0;
+        numTracksInFolder = mp3.getFolderTrackCount(myCard.folder);
+        Serial.print(numTracksInFolder);
+        Serial.print(F(" Dateien in Ordner "));
+        Serial.println(myCard.folder);
+  
+        // Hörspielmodus: eine zufällige Datei aus dem Ordner
+        if (myCard.mode == 1) {
+          Serial.println(F("Hörspielmodus -> zufälligen Track wiedergeben"));
+          currentTrack = random(1, numTracksInFolder + 1);
+          Serial.println(currentTrack);
+          mp3.playFolderTrack(myCard.folder, currentTrack);
+        }
+        // Album Modus: kompletten Ordner spielen
+        if (myCard.mode == 2) {
+          Serial.println(F("Album Modus -> kompletten Ordner wiedergeben"));
+          currentTrack = 1;
+          mp3.playFolderTrack(myCard.folder, currentTrack);
+        }
+        // Party Modus: Ordner in zufälliger Reihenfolge
+        if (myCard.mode == 3) {
+          Serial.println(
+              F("Party Modus -> Ordner in zufälliger Reihenfolge wiedergeben"));
+          currentTrack = random(1, numTracksInFolder + 1);
+          mp3.playFolderTrack(myCard.folder, currentTrack);
+        }
+        // Einzel Modus: eine Datei aus dem Ordner abspielen
+        if (myCard.mode == 4) {
+          Serial.println(
+              F("Einzel Modus -> eine Datei aus dem Odrdner abspielen"));
+          currentTrack = myCard.special;
+          mp3.playFolderTrack(myCard.folder, currentTrack);
+        }
+        // Hörbuch Modus: kompletten Ordner spielen und Fortschritt merken
+        if (myCard.mode == 5) {
+          Serial.println(F("Hörbuch Modus -> kompletten Ordner spielen und "
+                           "Fortschritt merken"));
+          currentTrack = EEPROM.read(myCard.folder);
+          mp3.playFolderTrack(myCard.folder, currentTrack);
+        }
+      }
+
+      // Neue Karte konfigurieren
+      else {
+        knownCard = false;
+        setupCard();
+      }
     }
   }
   
@@ -328,6 +326,14 @@ void loop() {
     Serial.println("Tag gone. Pausing music.");
     mp3.pause();
   }
+
+   // Halt PICC
+  mfrc522.PICC_HaltA();
+  // Stop encryption on PCD
+  mfrc522.PCD_StopCrypto1(); // Workaround from https://github.com/miguelbalboa/rfid/issues/352#issuecomment-431003051
+  mfrc522.PCD_Reset();
+  delay(100);
+  mfrc522.PCD_Init(); // Init MFRC522
     
 }
 
@@ -515,14 +521,6 @@ bool readCard(nfcTagObject *nfcTag) {
   nfcTag->folder = buffer[5];
   nfcTag->mode = buffer[6];
   nfcTag->special = buffer[7];
-
-  // Halt PICC
-  mfrc522.PICC_HaltA();
-  // Stop encryption on PCD
-  mfrc522.PCD_StopCrypto1(); // Workaround from https://github.com/miguelbalboa/rfid/issues/352#issuecomment-431003051
-  mfrc522.PCD_Reset();
-  delay(100);
-  mfrc522.PCD_Init(); // Init MFRC522
 
   return true;
 }
